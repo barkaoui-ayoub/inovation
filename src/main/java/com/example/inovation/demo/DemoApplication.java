64df3cb634aca515453648b55de882e42ce7e04c
barkaoui_work <barkaoui.ayoub@gmail.com>
2021-03-22 15:14:59 +0100

    test rebase

--- src/main/java/com/example/inovation/demo/DemoApplication.java
+++ src/main/java/com/example/inovation/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     // add on git
     // test
+    // add on local
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
